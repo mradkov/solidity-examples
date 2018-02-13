baeec751eee65debc669bdb83f888b5a09c64597
Milen Radkov <milenradkov@me.com>
2018-02-13 16:01:22 +0200

    Simple token
    Write a contract in Solidity that represents a simple token. The initial supply must be set at contract’s creation and this amount must be allocated to the address that creates the contract. Add a functionality that allows for transfers of tokens between the address of the contract’s creator and other addresses

---
+++ SimpleToken.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.19;
+
+
+contract Owned {
+    address public owner;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function Owned() public {
+        owner = msg.sender;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner returns (bool) {
+        owner = _newOwner;
+    }
+}
+
+contract SimpleToken is Owned {
+    
+    mapping(address => uint) public balanceOf;
+    
+    function SimpleToken (uint _initialSupply) public {
+        owner = msg.sender;
+        balanceOf[msg.sender] = _initialSupply;
+    }
+    
+    function trasfer(address _toAddress, uint _amount) public {
+        require(balanceOf[msg.sender] >= _amount);
+        require(balanceOf[_toAddress] + _amount >= balanceOf[_toAddress]);
+        balanceOf[msg.sender] -= _amount;
+        balanceOf[_toAddress] += _amount;
+    }
+    
+}
