5cfbed81c48f6f80e881f7bd256d1f395bfa52f8
Milen Radkov <milenradkov@me.com>
2018-02-15 13:47:28 +0200

    event multiple
    event multiple

---
+++ EventMultiple.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.19;
+
+// Write a contract in Solidity that contains a function
+// that receives a string and an address.
+// An event must be used that shows the values of these two variables. 
+
+contract EventMultiple {
+
+    address owner;
+
+    event LogInformation(string greeting, address owner);
+
+    function EventMultiple() public {
+        owner = msg.sender;
+    }
+
+    function showAddress(string greeting) public {
+        LogInformation(greeting, owner);
+    }
+
+}
